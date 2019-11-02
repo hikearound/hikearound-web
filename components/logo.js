704802de7ba7a0a9607e7bac588ebc6f8570f161
Patrick Dugan <dugan.pat@gmail.com>
2019-11-02 08:30:00 -0700

    Nukes passed prop type

--- components/logo.js
+++ components/logo.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
-const propTypes = {
-    dimension: PropTypes.number,
-};
-
-const defaultProps = {
-    dimension: 125,
-};
-
-const Logo = ({ dimension }) => (
-    <LogoImage
-        dimension={dimension}
-        src='/images/logo.svg'
-        alt='Hikearound logo'
-    />
-);
-
-Logo.propTypes = propTypes;
-Logo.defaultProps = defaultProps;
+const Logo = () => <LogoImage src='/images/logo.svg' alt='Hikearound logo' />;
 
 export default Logo;
 
 const LogoImage = styled.img`
-    height: ${(props) => props.dimension}px;
-    width: ${(props) => props.dimension}px;
+    height: 125px;
+    width: 125px;
 `;
