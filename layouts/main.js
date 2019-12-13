ec2716626d21bc975c401e146aaef523a7538885
Patrick Dugan <dugan.pat@gmail.com>
2019-12-12 22:36:36 -0800

    Added nprogress loading bar

--- layouts/main.js
+++ layouts/main.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { createGlobalStyle } from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Header from '../components/page/Header';
 import Footer from '../components/page/Footer';
 import { typeface, fontSize } from '../constants/type';
 import { grid } from '../constants/dimensions';
 import colors from '../constants/colors';
-import '../css/reset.css';
 import { device } from '../constants/breakpoints';
+
+import '../css/reset.css';
+import '../css/nprogress.css';
 
 const columnType = [PropTypes.object, PropTypes.array];
 
@@ -29,6 +33,15 @@ class Page extends React.PureComponent {
     constructor(props) {
         super(props);
         const { title } = this.props;
+
+        Router.events.on('routeChangeStart', () => {
+            NProgress.start();
+        });
+
+        Router.events.on('routeChangeComplete' || 'routeChangeError', () => {
+            NProgress.done();
+        });
+
         this.state = {
             pageTitle: title,
         };
