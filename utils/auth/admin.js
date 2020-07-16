cd33dbacd8289abb2e372c54f0148f3fbcf031c0
Patrick Dugan <dugan.pat@gmail.com>
2020-07-16 11:02:33 -0400

    Again

--- utils/auth/admin.js
+++ utils/auth/admin.js
@@ -1,9 +1,15 @@
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../keys/firebase.json';
 
 export function initAdmin() {
+    let privateKey = process.env.FIREBASE_PRIVATE_KEY;
+    privateKey = privateKey.replace(/\\n/g, '\n');
+
     const config = {
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert({
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey,
+        }),
         databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     };
 
