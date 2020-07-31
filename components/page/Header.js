1b872955bee7a7327e22f89527d32eb5d6642a8c
Patrick Dugan <dugan.pat@gmail.com>
2020-07-31 07:15:08 -0400

    Nits

--- components/page/Header.js
+++ components/page/Header.js
@@ -70,7 +70,7 @@ class Header extends React.PureComponent {
                     <title>{title}</title>
                     <meta
                         name='viewport'
-                        content='width=device-width, initial-scale=1'
+                        content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover'
                     />
                     <meta charSet='utf-8' />
                     {this.renderOpenGraph()}
