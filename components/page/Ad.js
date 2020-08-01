d891117e4b5dcf6e13d2f6a27669c5d9cb1f1f8a
Patrick Dugan <dugan.pat@gmail.com>
2020-08-01 08:56:38 -0400

    Props on ad unit

--- components/page/Ad.js
+++ components/page/Ad.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { SecondaryHeading } from '../../styles/headings';
 import { Card } from '../../styles/card';
 import { withTranslation } from '../../utils/i18n';
+
+const propTypes = {
+    client: PropTypes.string,
+    slot: PropTypes.string,
+    format: PropTypes.string,
+    responsive: PropTypes.bool,
+};
+
+const defaultProps = {
+    client: 'ca-pub-0600907327424384',
+    slot: '9374444460',
+    format: 'rectangle',
+    responsive: true,
+};
 
 class Ad extends React.PureComponent {
     componentDidMount() {
@@ -9,14 +24,16 @@ class Ad extends React.PureComponent {
     }
 
     renderAd = () => {
+        const { client, slot, format, responsive } = this.props;
+
         return (
             <ins
                 className='adsbygoogle'
                 style={{ display: 'block' }}
-                data-ad-client='ca-pub-0600907327424384'
-                data-ad-slot='9374444460'
-                data-ad-format='rectangle'
-                data-full-width-responsive='true'
+                data-ad-client={client}
+                data-ad-slot={slot}
+                data-ad-format={format}
+                data-full-width-responsive={responsive}
             />
         );
     };
@@ -32,5 +49,8 @@ class Ad extends React.PureComponent {
         );
     }
 }
+
+Ad.propTypes = propTypes;
+Ad.defaultProps = defaultProps;
 
 export default withTranslation('common')(Ad);
