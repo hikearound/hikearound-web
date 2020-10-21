36772480d752d5f23352b1d9ca3daece08753c67
Patrick Dugan <dugan.pat@gmail.com>
2020-10-21 14:08:31 -0400

    Better carousel order

--- constants/data.js
+++ constants/data.js
@@ -1,13 +1,13 @@
 export const hikeData = [
     {
-        name: 'Discovery Park Loop',
-        city: 'Seattle, WA',
-        distance: 4.34,
-        difficulty: 'Moderate',
-        elevation: 423,
+        name: 'Lands End Trail',
+        city: 'San Francisco, CA',
+        distance: 3.31,
+        difficulty: 'Easy',
+        elevation: 641,
         image:
-            'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/hikes%2FMRHv2dPElugz31v9NKov%2Fimages%2Fcovers%2F1_750x750.jpg?alt=media&token=b327168d-87ff-449f-96cc-fdb7b653c0ae',
-        id: 'MRHv2dPElugz31v9NKov',
+            'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/hikes%2F3pg1qXxCnXaMZ7Tr8Xzm%2Fimages%2Fcovers%2F1_750x750.jpg?alt=media&token=46d6814e-d286-4f7d-b316-59e6e122730c',
+        id: '3pg1qXxCnXaMZ7Tr8Xzm',
     },
     {
         name: 'Green Lake Loop',
@@ -20,16 +20,6 @@ export const hikeData = [
         id: 'kh1Joa3G6Fcj1veq9YDm',
     },
     {
-        name: 'Lands End Trail',
-        city: 'San Francisco, CA',
-        distance: 3.31,
-        difficulty: 'Easy',
-        elevation: 641,
-        image:
-            'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/hikes%2F3pg1qXxCnXaMZ7Tr8Xzm%2Fimages%2Fcovers%2F1_750x750.jpg?alt=media&token=46d6814e-d286-4f7d-b316-59e6e122730c',
-        id: '3pg1qXxCnXaMZ7Tr8Xzm',
-    },
-    {
         name: 'Golden Gardens Circuit',
         city: 'Seattle, WA',
         distance: 1.8,
@@ -40,14 +30,14 @@ export const hikeData = [
         id: 'xn9c36q0ptDvn8DXh8x2',
     },
     {
-        name: 'Joaquin Miller Park Loop',
-        city: 'Oakland, CA',
-        distance: 3.3,
+        name: 'Discovery Park Loop',
+        city: 'Seattle, WA',
+        distance: 4.34,
         difficulty: 'Moderate',
-        elevation: 514,
+        elevation: 423,
         image:
-            'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/hikes%2FLJBu2eBXzs6xKJ0oaMOw%2Fimages%2Fcovers%2F2_750x750.jpg?alt=media&token=d894bdd0-5f54-44d1-a33d-ec3b367bc496',
-        id: 'LJBu2eBXzs6xKJ0oaMOw',
+            'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/hikes%2FMRHv2dPElugz31v9NKov%2Fimages%2Fcovers%2F1_750x750.jpg?alt=media&token=b327168d-87ff-449f-96cc-fdb7b653c0ae',
+        id: 'MRHv2dPElugz31v9NKov',
     },
     {
         name: 'Punch Bowl Lake Loop',
@@ -58,6 +48,16 @@ export const hikeData = [
         image:
             'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/hikes%2F97PXRmn0OrwGC31UcQcI%2Fimages%2Fcovers%2F1_750x750.jpg?alt=media&token=545d2143-805e-4659-ab74-a97a10a05d8c',
         id: '97PXRmn0OrwGC31UcQcI',
+    },
+    {
+        name: 'Joaquin Miller Park Loop',
+        city: 'Oakland, CA',
+        distance: 3.3,
+        difficulty: 'Moderate',
+        elevation: 514,
+        image:
+            'https://firebasestorage.googleapis.com/v0/b/hikearound-14dad.appspot.com/o/hikes%2FLJBu2eBXzs6xKJ0oaMOw%2Fimages%2Fcovers%2F2_750x750.jpg?alt=media&token=d894bdd0-5f54-44d1-a33d-ec3b367bc496',
+        id: 'LJBu2eBXzs6xKJ0oaMOw',
     },
 ];
 
